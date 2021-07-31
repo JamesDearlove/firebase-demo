c294172bf60b5f989fe95ca7a74a42445c8b5fa5
James Dearlove <jamesdearlove@outlook.com>
2021-07-31 21:43:14 +1000

    Building up timeline components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CssBaseline } from "@material-ui/core";
+import { Container, CssBaseline, Grid } from "@material-ui/core";
 import MenuBar from "./components/MenuBar";
 import Timeline from "./components/Timeline";
 import Sidebar from "./components/Sidebar";
@@ -8,8 +8,16 @@ function App() {
   return (
     <CssBaseline>
       <MenuBar />
-      <Timeline />
-      <Sidebar />
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item xs={8}>
+            <Timeline />
+          </Grid>
+          <Grid item xs={4}>
+            <Sidebar />
+          </Grid>
+        </Grid>
+      </Container>
     </CssBaseline>
   );
 }
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,7 +1,115 @@
 import React from "react";
+import {
+  Avatar,
+  Box,
+  IconButton,
+  makeStyles,
+  Typography,
+  Paper,
+} from "@material-ui/core";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import CommentIcon from "@material-ui/icons/Comment";
+
+const useStyles = makeStyles((theme) => ({
+  postPaper: {
+    marginBottom: theme.spacing(2),
+    padding: theme.spacing(2),
+  },
+  timeline: {
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(2),
+  },
+  authorBox: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: theme.spacing(1),
+  },
+  authorText: {
+    marginLeft: theme.spacing(1),
+  },
+  actionsBox: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+}));
+
+const PostActions = (props: TimelinePostProps) => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.actionsBox}>
+      <IconButton>
+        <CommentIcon />
+      </IconButton>
+      <Box className={classes.actionsBox}>
+        <IconButton>
+          <FavoriteBorderIcon />
+        </IconButton>
+        <Typography>{props.likes}</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+interface TimelinePostProps {
+  content: string;
+  author: string;
+  authorId: string;
+  likes: number;
+  comments: string[];
+}
+
+const TimelinePost = (props: TimelinePostProps) => {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.postPaper}>
+      <Box className={classes.authorBox}>
+        <Avatar alt={props.author}>{props.author[0]}</Avatar>
+        <Typography className={classes.authorText}>{props.author}</Typography>
+      </Box>
+      <Typography>{props.content}</Typography>
+      <PostActions {...props} />
+    </Paper>
+  );
+};
+
+const tempPosts: TimelinePostProps[] = [
+  {
+    content: "You should watch The Owl House.",
+    author: "James",
+    authorId: "fakeid",
+    comments: [],
+    likes: 5,
+  },
+  {
+    content: "You should watch Amphibia.",
+    author: "James",
+    authorId: "fakeid",
+    comments: [],
+    likes: 5,
+  },
+  {
+    content: "You should watch The Dragon Prince.",
+    author: "James",
+    authorId: "fakeid",
+    comments: [],
+    likes: 5,
+  },
+];
 
 const Timeline = () => {
-  return <></>;
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.timeline}>
+      {tempPosts.map((post) => (
+        <TimelinePost {...post} />
+      ))}
+    </Paper>
+  );
 };
 
 export default Timeline;
